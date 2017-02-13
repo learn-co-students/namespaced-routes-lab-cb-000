5cefd89b7affd5c3b0143f46c3beabacf9789abf
sleeper <survivingknowledge@gmail.com>
2017-02-13 13:17:34 -0500

    Done.

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -8,7 +8,14 @@ class ArtistsController < ApplicationController
   end
 
   def new
-    @artist = Artist.new
+
+    @preference = Preference.first
+
+    if !@preference.allow_create_artists
+      redirect_to artists_path, alert: "Cannot Create New Artists."
+    else
+      @artist = Artist.new
+    end
   end
 
   def create
