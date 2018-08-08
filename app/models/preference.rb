433230679e4014d143ca531ac8ee37208890a6a3
Alex Segers <a.segers.dev@gmail.com>
2018-08-08 02:39:17 +0000

    Done.

--- app/models/preference.rb
+++ app/models/preference.rb
@@ -1,20 +1,2 @@
-class Preference < ActiveRecord::base
-  attr_reader :allow_create_artists, :artist_sort_order, :allow_create_songs, :song_sort_order
-
-  def allow_create_artists(bool = true)
-
-  end
-
-  def artist_sort_order(order = "DESC")
-
-  end
-
-
-  def allow_create_songs(bool = true)
-
-  end
-
-  def song_sort_order(order = "DESC")
-
-  end
+class Preference < ActiveRecord::Base
 end
