2d0b5b47bd5e70387efe340d2fb384488856645c
Adam Urbina <adamurbina@gmail.com>
2018-11-06 21:12:42 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -25,7 +25,11 @@ class SongsController < ApplicationController
   end
 
   def new
-    @song = Song.new
+    if validate_pref?
+      @song = Song.new
+    else
+      redirect_to songs_path
+    end
   end
 
   def create
@@ -66,5 +70,10 @@ class SongsController < ApplicationController
   def song_params
     params.require(:song).permit(:title, :artist_name)
   end
-end
 
+  def validate_pref?
+    pref = Preference.all.last
+    pref.allow_create_songs == TRUE
+  end
+
+end
---
+++ db/migrate/20181105001038_add_columns_to_preference.rb
@@ -0,0 +1,7 @@
+class AddColumnsToPreference < ActiveRecord::Migration
+  def change
+    add_column :preferences, :artist_sort_order, :string
+    add_column :preferences, :song_sort_order, :string
+    add_column :preferences, :allow_create_songs, :boolean
+  end
+end
