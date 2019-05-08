6fa6f332860dc08eab51b09d9da6ee9865fea292
Konstantinos Pediaditakis <pediadk@gmail.com>
2019-05-08 17:21:10 +0000

    Done.

--- app/controllers/admin/preferences_controller.rb
+++ app/controllers/admin/preferences_controller.rb
@@ -1,2 +1,14 @@
 class Admin::PreferencesController < ApplicationController
+  def index
+    @prefs = Preference.first
+  end
+
+  def edit
+    @prefs = Preference.first
+  end
+
+  def update
+    Preference.first.update(params[:preference])
+    redirect_to admin_preferences
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   namespace :admin do
-    resources :preferences
+    resources :preferences, only: [:index, :edit]
   end
   resources :artists do
     resources :songs, only: [:index, :show]
